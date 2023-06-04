d14b66549a31a732f77e888928e410aeeff0ce8d
Samuellekpor <samuellekpor@gmail.com>
2023-06-04 23:19:13 +0000

    Display the chosen rating on the thank you page

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const submit = document.querySelector('#submit-button');
 const rating_state = document.querySelector('.rating-state');
 const thank_you_state = document.querySelector('.thank-you-state');
+const result = document.getElementById('result');
+const ratings = document.querySelectorAll('.btn');
 
-submit.addEventListener('click', ()=>{
+submit.addEventListener('click', () => {
   rating_state.style.display = 'none';
   thank_you_state.style.display = 'block';
+})
+
+ratings.forEach((rate) => {
+  rate.addEventListener('click', () => {
+    result.innerHTML = rate.innerHTML;
+  })
 })
